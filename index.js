ee24a06ceb71eebbef553ef0fa27c670dee2a9f8
Alvee Akand <alvee.akand@outlook.com>
2021-12-17 12:37:56 -0500

    added solutions

--- index.js
+++ index.js
@@ -1,37 +1,89 @@
-function forEach() {
-
+function forEach(array, callback) {
+  for (let i = 0; i < array.length; i++) {
+    callback(array[i], i, array);
+  }
 }
 
-function map() {
-
+function map(array, callback) {
+  const resArr = [];
+  for (let i = 0; i < array.length; i++) {
+    resArr.push(callback(array[i], i, array));
+  }
+  return resArr;
 }
 
-function filter() {
-
+function filter(array, callback) {
+  const resArr = [];
+  for (let i = 0; i < array.length; i++) {
+    if (callback(array[i], i, array) === true) {
+      resArr.push(array[i]);
+    }
+  }
+  return resArr;
 }
 
-function every() {
-
+function every(array, callback) {
+  for (let i = 0; i < array.length; i++) {
+    if (callback(array[i], i, array) === false) {
+      return false;
+    }
+  }
+  return true;
 }
 
-function some() {
-
+function some(array, callback) {
+  for (let i = 0; i < array.length; i++) {
+    if (callback(array[i], i, array) === true) {
+      return true;
+    }
+  }
+  return false;
 }
 
-function majority() {
-
+function majority(array, callback) {
+  const majorityCount = Math.ceil(array.length / 2);
+  let trueCount = 0;
+  for (const el of array) {
+    if (callback(el)) {
+      trueCount++;
+      if (trueCount >= majorityCount) return true;
+    }
+  }
+  return false;
 }
 
-function once() {
-
+function once(callback) {
+  let called = false;
+  let cachedOutput;
+  function innerOnce(...args) {
+    if (!called) {
+      cachedOutput = callback(...args);
+      called = true;
+      return cachedOutput;
+    }
+    return cachedOutput;
+  }
+  return innerOnce;
 }
 
-function groupBy() {
-
+function groupBy(array, callback) {
+  let result = {};
+  for (const el of array) {
+      let key = callback(el);
+      if (!result.hasOwnProperty(key)) {
+          result[key] = [];
+      }
+      result[key].push(el);
+  }
+  return result;
 }
 
-function arrayToObject() {
-
+function arrayToObject(array, callback) {
+  const res = {};
+  for (const el of array) {
+    res[el] = callback(el);
+  }
+  return res;
 }
 
 module.exports = { 
