efd8b8ad2fd370e5be492d6b38317a32c482abef
Alvee Akand <alvee.akand@outlook.com>
2021-12-17 09:27:33 -0500

    added starter code

---
+++ index.js
@@ -0,0 +1,47 @@
+function forEach() {
+
+}
+
+function map() {
+
+}
+
+function filter() {
+
+}
+
+function every() {
+
+}
+
+function some() {
+
+}
+
+function majority() {
+
+}
+
+function once() {
+
+}
+
+function groupBy() {
+
+}
+
+function arrayToObject() {
+
+}
+
+module.exports = { 
+  forEach, 
+  map, 
+  filter, 
+  every, 
+  some, 
+  majority,
+  once,
+  groupBy,
+  arrayToObject,
+};
