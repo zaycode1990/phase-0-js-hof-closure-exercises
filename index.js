6b772c23796ec07bc8e868adfc5dbd2513c51d70
Isaiah <ijandicos90@gmail.com>
2022-03-25 22:00:14 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,37 +1,81 @@
-function forEach() {
-
+function forEach(arr,callback) {
+for(let i = 0; i < arr.length; i++){
+  callback(arr[i],i, arr)
+}
 }
 
-function map() {
-
+function map(arr,callback) {
+  const newArr = []
+    for(let el of arr){
+      newArr.push(callback(el))
+    }
+  return newArr
 }
 
-function filter() {
-
+function filter(arr, callback) {
+  const newArr = []
+  for(let el of arr){
+    if (callback(el)){
+      newArr.push(el)
+    }
+  }
+return newArr
 }
 
-function every() {
-
+function every(arr, callback) {
+  for(let el of arr){
+    if(!callback(el)){
+      return false
+    }
+  }
+return true
 }
 
-function some() {
-
+function some(arr,callback) {
+  for(let el of arr){
+    if(callback(el)){
+      return true
+    }
+  }
+return false
 }
 
-function majority() {
-
+function majority(arr, callback) { 
+  let trueCount = 0
+  for(let i = 0; i < arr.length; i++){
+    if(callback(arr[i])){
+      trueCount++
+      if(trueCount>arr.length/2){
+        return true
+      }
+    }
+  }
+return false
 }
 
-function once() {
-
+function once(callback) {
+const _ = require('lodash');
+return _.once(callback)
 }
 
-function groupBy() {
-
+function groupBy(arr, callback) {
+const newObj = {}
+for(let el of arr){
+  if(newObj[callback(el)]){
+    newObj[callback(el)].push(el)
+  } else {
+    newObj [callback(el)] = [el]
+  }
+}
+return newObj
 }
 
-function arrayToObject() {
-
+function arrayToObject(arr,callback) {
+  const newObj = {}
+  for(let el of arr) {
+    newObj[el] = callback(el)
+  }
+return newObj
 }
 
 module.exports = { 
